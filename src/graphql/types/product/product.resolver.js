6b6d6e8c5c5ec6736435f1f2a8615277a4dd127b
Hernani <hernani.luiz@gmail.com>
2020-02-25 18:47:45 -0300

    evitando erro em amount e retornado valor

--- src/graphql/types/product/product.resolver.js
+++ src/graphql/types/product/product.resolver.js
@@ -17,14 +17,15 @@ const getProducType = async prod => {
     shipping
   } = prod;
 
-  const { free_shipping } = shipping;
-  const { amount } = installments;
-  const { pictures, sold_quantity } = await getProduct(id);
+  const { free_shipping = 0 } = shipping || {};
+  const { amount = 0 } = installments || {};
+  const { pictures, sold_quantity, price: value } = await getProduct(id);
 
   return {
     id,
     title,
     price: {
+      value,
       currency,
       decimals: await getDecimals(currency),
       amount
@@ -32,16 +33,17 @@ const getProducType = async prod => {
     condition,
     free_shipping,
     picture: pictures[0].url,
-    sold_quantity: sold_quantity
+    sold_quantity
   };
 };
 
 const getProduct = async id => {
   const { data } = await Http.get(`items/${id}`);
-  const { pictures, sold_quantity } = data;
+  const { pictures, sold_quantity, price } = data;
   return {
     pictures,
-    sold_quantity
+    sold_quantity,
+    price
   };
 };
 
@@ -55,8 +57,8 @@ const getAllCategories = filters =>
     .reduce((a, b) => [a + b], []);
 
 const produsearchProductsResolver = async term => {
-  console.log(term, 'hhh');
-  const { data } = await Http.get(`sites/MLA/search?q=${term}`);
+  const { term: productToSearch } = term;
+  const { data } = await Http.get(`sites/MLA/search?q=${productToSearch}`);
   const { results, available_filters } = data;
   const items = results.map(async prod => getProducType(prod));
 
